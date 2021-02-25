3fe4cd29bdef6b468681a18e9237a940ef6f3fce
wangq <Qin.Wang@verisilicon.com>
2021-02-25 15:19:55 +0800

    avcodec/h264dec: add H264 VPE HW decoder

--- libavcodec/allcodecs.c
+++ libavcodec/allcodecs.c
@@ -829,6 +829,12 @@ extern AVCodec ff_vp9_mediacodec_decoder;
 extern AVCodec ff_vp9_qsv_decoder;
 extern AVCodec ff_vp9_vaapi_encoder;
 extern AVCodec ff_vp9_qsv_encoder;
+extern AVCodec ff_h264_vpe_decoder;
+extern AVCodec ff_hevc_vpe_decoder;
+extern AVCodec ff_vp9_vpe_decoder;
+extern AVCodec ff_h264_vpe_encoder;
+extern AVCodec ff_hevc_vpe_encoder;
+extern AVCodec ff_vp9_vpe_encoder;
 
 // The iterate API is not usable with ossfuzz due to the excessive size of binaries created
 #if CONFIG_OSSFUZZ
