9141a8df977377362628a5671dab565244925bb5
wangq <Qin.Wang@verisilicon.com>
2021-02-25 15:20:18 +0800

    avfilter/spliter: add VPE spliter filter

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -460,6 +460,8 @@ extern AVFilter ff_vf_yaepblur;
 extern AVFilter ff_vf_zmq;
 extern AVFilter ff_vf_zoompan;
 extern AVFilter ff_vf_zscale;
+extern AVFilter ff_vf_spliter_vpe;
+extern AVFilter ff_vf_pp_vpe;
 
 extern AVFilter ff_vsrc_allrgb;
 extern AVFilter ff_vsrc_allyuv;
